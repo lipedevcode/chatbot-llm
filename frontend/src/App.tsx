0a7f7f0b5472a15b932e1fe5a7a36f91dfba4a5d
Bernardo <joseberk2@gmail.com>
2026-05-29 15:17:15 -0300

    feat: initialize frontend project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <main className="bg-brown-dark">
+        <h1 className="text-blue-300"> Hello world </h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
