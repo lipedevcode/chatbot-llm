3349a50efd4c549b181a9faf782945674516100c
Luis Felipe <luis.castro@ufpi.edu.br>
2026-05-30 18:27:56 -0300

    feat: implement session creation mechanism

--- backend/src/main/java/com/chatbotllm/backend/Service.java
+++ backend/src/main/java/com/chatbotllm/backend/Service.java
@@ -2,20 +2,15 @@ package com.chatbotllm.backend;
 
 import com.chatbotllm.backend.inteface.personas.GenericAssistant;
 import com.chatbotllm.backend.model.Session;
-import com.chatbotllm.backend.repository.SessionRepository;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.ChatMessageDeserializer;
-import dev.langchain4j.data.message.ChatMessageSerializer;
+import com.chatbotllm.backend.repositories.SessionRepository;
+import com.chatbotllm.backend.utils.PersistentChatMemoryStore;
 import dev.langchain4j.memory.chat.ChatMemoryProvider;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.store.memory.chat.ChatMemoryStore;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
@@ -46,53 +41,18 @@ public class Service {
                 .chatMemoryProvider(chatMemoryProvider)
                 .build();
 
-        Optional<Session> session1 = sessionRepository.findByMemoryId(1L);
-        if (session1.isEmpty()) sessionRepository.save(new Session(1L, ""));
-        Optional<Session> session2 = sessionRepository.findByMemoryId(2L);
-        if (session2.isEmpty()) sessionRepository.save(new Session(2L, ""));
-        System.out.println(genericAssistant.chat(1L, "What is my name?"));
-        System.out.println(genericAssistant.chat(2L, "What is my name?"));
+        this.createSessionIfNotExists(id);
 
-        return "";
-    }
-}
-class PersistentChatMemoryStore implements ChatMemoryStore {
-
-    private final SessionRepository sessionRepository;
-
-
-    public PersistentChatMemoryStore(SessionRepository sessionRepository) {
-        this.sessionRepository = sessionRepository;
+        return genericAssistant.chat(id, userMessage);
     }
 
-    @Override
-    @Transactional(readOnly = true)
-    public List<ChatMessage> getMessages(Object memoryId) {
-        Session session = sessionRepository.findById((Long) memoryId).orElseThrow(
-                () -> new RuntimeException("Erro: session_id #" + memoryId + " não encontrado")
-        );
-        String messages = session.getMessages();
-        if (messages == null || messages.isBlank()) return List.of();
-        return ChatMessageDeserializer.messagesFromJson(messages);
-    }
-
-    @Override
-    @Transactional
-    public void updateMessages(Object memoryId, List<ChatMessage> messages) {
-        Session session = sessionRepository.findById((Long) memoryId).orElseThrow(
-                () -> new RuntimeException("Erro: session_id #" + memoryId + " não encontrado")
-        );
-        String messagesToJson = ChatMessageSerializer.messagesToJson(messages);
-        session.setMessages(messagesToJson);
-        sessionRepository.save(session);
-    }
-
-    @Override
-    @Transactional
-    public void deleteMessages(Object memoryId) {
-        Session session = sessionRepository.findById((Long) memoryId).orElseThrow(
-                () -> new RuntimeException("Erro: session_id #" + memoryId + " não encontrado")
-        );
-        sessionRepository.delete(session);
+    private void createSessionIfNotExists(Long memoryId) {
+        Optional<Session> sessionOptional = sessionRepository.findByMemoryId(memoryId);
+        if (sessionOptional.isEmpty()) {
+            Session session = new Session();
+            session.setMemoryId(memoryId);
+            session.setMessages("");
+            sessionRepository.save(session);
+        }
     }
 }
