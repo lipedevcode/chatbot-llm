487a699a0b6311d099860138741d4016e1e1af9b
bernardo <joseberk2@gmail.com>
2026-06-04 10:36:11 -0300

    refactor: change database structure of some entities

--- frontend/src/interfaces/database.ts
+++ frontend/src/interfaces/database.ts
@@ -1,29 +1,23 @@
 export interface Response {
-  id: number;
   text: string;
 }
 
 export interface Session {
-  memoryId: number;
   messages: string;
 }
 
 export interface History {
-  id: number;
+  id?: number | null;
   prompts?: Prompt[] | null;
-  session: Session;
+  session?: Session;
 }
 
 export interface Usuario {
   id: number;
-  history?: History | null;
-  prompts?: Prompt[] | null;
+  subject: string; // username como hash de palavra aleatória
 }
 
 export interface Prompt {
-  id: number;
   text: string;
   response?: Response | null;
-  history?: History | null;
-  usuario?: Usuario | null;
 }
