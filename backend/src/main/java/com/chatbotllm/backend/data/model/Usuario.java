3a069cafc8998d31345cd53ce51f5ec11544225a
Luis Felipe <luis.castro@ufpi.edu.br>
2026-06-03 22:04:48 -0300

    remove unused import

--- backend/src/main/java/com/chatbotllm/backend/data/model/Usuario.java
+++ backend/src/main/java/com/chatbotllm/backend/data/model/Usuario.java
@@ -1,6 +1,5 @@
 package com.chatbotllm.backend.data.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.jspecify.annotations.Nullable;
