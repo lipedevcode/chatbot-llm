169aa6a02b3310ce76ae5d09604f766a22f264eb
Luis Felipe <luis.castro@ufpi.edu.br>
2026-07-02 20:58:57 -0300

    feat: Add support for file uploads in chat messages

--- backend/src/main/java/com/chatbotllm/backend/service/ChatService.java
+++ backend/src/main/java/com/chatbotllm/backend/service/ChatService.java
@@ -1,33 +1,69 @@
 package com.chatbotllm.backend.service;
 
 import com.chatbotllm.backend.data.dto.HistoryDto;
+import com.chatbotllm.backend.data.model.File;
 import com.chatbotllm.backend.data.model.History;
 import com.chatbotllm.backend.data.request.SendChatMessageRequest;
 import com.chatbotllm.backend.data.response.SendChatMessageResponse;
 import com.chatbotllm.backend.inteface.personas.GenericAssistant;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @org.springframework.stereotype.Service
 @RequiredArgsConstructor
 public class ChatService {
 
     private final HistoryService historyService;
     private final InteractionService interactionService;
+    private final FileService fileService;
     private final GenericAssistant genericAssistant;
 
     public SendChatMessageResponse sendChatMessage(SendChatMessageRequest sendChatMessageRequest) {
-        History history = this.historyService.resolveHistory(sendChatMessageRequest.getHistoryId());
+        return this.sendChatMessage(sendChatMessageRequest.getHistoryId(), sendChatMessageRequest.getUserMessage());
+    }
 
-        if (history.getPrompts() == null)
+    public SendChatMessageResponse sendChatMessage(Long historyId, String userMessage){
+        History history = this.historyService.resolveHistory(historyId);
+
+        if (history.getPrompts() == null){
             history.setPrompts(new ArrayList<>());
+            }
 
         Long sessionMemoryId = history.getSession().getMemoryId();
 
-        String aiMessage = genericAssistant.chat(sessionMemoryId, sendChatMessageRequest.getUserMessage());
+        String aiMessage = genericAssistant.chat(sessionMemoryId, userMessage);
 
-        this.interactionService.saveInteraction(sendChatMessageRequest.getUserMessage(), aiMessage, history);
+        this.interactionService.saveInteraction(userMessage, aiMessage, history);
+
+        return SendChatMessageResponse.builder()
+                .aiMessage(aiMessage)
+                .history(HistoryDto.fromHistory(history.getId(), history.getPrompts(), history.getSession()))
+                .build();
+    }
+
+    public SendChatMessageResponse sendChatMessage(Long historyId, String userMessage, List<MultipartFile> multipartFiles){
+        History history = this.historyService.resolveHistory(historyId);
+
+        if (history.getPrompts() == null) {
+            history.setPrompts(new ArrayList<>());
+        }
+
+        Long sessionMemoryId = history.getSession().getMemoryId();
+
+        List<File> files = this.fileService.createFromMultipartFiles(multipartFiles);
+
+        List<String> extractedTexts = this.fileService.getTextsFromFiles(files);
+
+        String userMessageWithFiles = String.join("\n\n", extractedTexts) +  "\n\n" + "## Pergunta do usuário: " + userMessage;
+
+        String aiMessage = genericAssistant.chat(sessionMemoryId, userMessageWithFiles);
+
+        this.interactionService.saveInteraction(userMessageWithFiles, aiMessage, history, files);
 
         return SendChatMessageResponse.builder()
                 .aiMessage(aiMessage)
