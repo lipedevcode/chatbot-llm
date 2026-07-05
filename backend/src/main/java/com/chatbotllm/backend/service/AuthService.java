5b27da7604f7bb6f912f6c28389b981a3e4403ab
Luis Felipe <luis.castro@ufpi.edu.br>
2026-07-04 22:57:45 -0300

    feat: Implement user authentication with login and signup endpoints

--- backend/src/main/java/com/chatbotllm/backend/service/AuthService.java
+++ backend/src/main/java/com/chatbotllm/backend/service/AuthService.java
@@ -1,6 +1,8 @@
 package com.chatbotllm.backend.service;
 
 import com.chatbotllm.backend.data.model.Usuario;
+import com.chatbotllm.backend.data.request.SigninRequest;
+import com.chatbotllm.backend.data.request.SignupRequest;
 import com.chatbotllm.backend.repositories.UsuarioRepository;
 import com.chatbotllm.backend.security.JwtService;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +22,29 @@ public class AuthService {
     private final UsuarioRepository usuarioRepository;
     private final JwtService jwtService;
 
-    public String signup(){
-        Usuario usuario = new Usuario();
-        usuario.setSubject(hash(Instant.now().toString()));
+    public String signup(SignupRequest signupRequest) {
+        this.validaSignup(signupRequest);
+        Instant createdAt = Instant.now();
+        Usuario usuario = Usuario.builder()
+                .username(signupRequest.getUsername())
+                .email(signupRequest.getEmail())
+                .password(hash(signupRequest.getPassword()))
+                .nome(signupRequest.getNome())
+                .createdAt(createdAt)
+                .build();
         usuarioRepository.save(usuario);
+
+        return jwtService.generateToken(usuario);
+    }
+
+    public String login(SigninRequest signinRequest) {
+        Usuario usuario = usuarioRepository.findByEmail(signinRequest.getEmail())
+                .orElseThrow(() -> new RuntimeException("Email #" + signinRequest.getEmail() + " não encontrado"));
+
+        String password = hash(signinRequest.getPassword());
+        if (!usuario.getPassword().equals(password)) {
+            throw new RuntimeException("Senha incorreta");
+        }
 
         return jwtService.generateToken(usuario);
     }
@@ -34,17 +55,17 @@ public class AuthService {
             throw new RuntimeException("Nenhum usuário autenticado encontrado");
         }
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        return usuarioRepository.findBySubject(userDetails.getUsername()).
+        return usuarioRepository.findByUsername(userDetails.getUsername()).
                 orElseThrow(() -> new RuntimeException("Usuário #" + userDetails.getUsername() + " não encontrado"));
     }
 
-    private String hash(String subject){
+    private String hash(String element){
         try {
             // Instancia o algoritmo desejado (SHA-256, MD5, SHA-512, etc.)
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
 
             // Converte a string para bytes e gera o hash
-            byte[] hashBytes = digest.digest(subject.getBytes());
+            byte[] hashBytes = digest.digest(element.getBytes());
 
             // Converte o array de bytes para uma representação em String hexadecimal
             StringBuilder hexString = new StringBuilder();
@@ -58,6 +79,15 @@ public class AuthService {
 
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Erro ao gerar o hash", e);
+        }
+    }
+
+    private void validaSignup(SignupRequest signupRequest) {
+        if (this.usuarioRepository.existsUsuarioByUsername(signupRequest.getUsername())) {
+            throw new RuntimeException("Username já existe");
+        }
+        if (this.usuarioRepository.existsUsuarioByEmail(signupRequest.getEmail())){
+            throw new RuntimeException("Email já existe");
         }
     }
 }
